5a04528812a386ecc294b66e161a986cd66a4906
hyx <123@123.com>
2017-03-12 10:45:02 +0800

    修改index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,16 @@
 * @Author: hyx
 * @Date:   2017-03-11 10:17:35
 * @Last Modified by:   hyx
-* @Last Modified time: 2017-03-11 10:17:35
+* @Last Modified time: 2017-03-12 10:42:05
 */
 
-'use strict';
+window.onresize = function(){
+	if(document.body.clientWidth < 900){
+		$(".hyx_banner li").css("padding","10px");
+		console.log(1);
+		
+	}
+}
+// console.log($(".hyx_banner li").css("padding"));
+// $(".hyx_banner li").css("padding","10px");
+// console.log($(".hyx_banner li").css("padding"));
